50f4bfca56003762bf4b7e81e2f11a4d92b3fbe5
nicolsandovalu <nicolsandovalu@gmail.com>
2026-07-15 19:14:00 -0400

    Se actualiza vite.config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/proyecto_minibanco/',
   plugins: [react()],
   test: {
     globals: true,
